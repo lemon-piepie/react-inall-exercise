f881418baf206d365033172d58d502a4145e8d5a
lemon-piepie <purplechilde@163.com>
2020-08-13 12:20:35 +0800

    finish counter function

--- src/page/Counter.js
+++ src/page/Counter.js
@@ -3,6 +3,39 @@ import { Link } from 'react-router-dom';
 import '../style/Counter.css';
 
 class Counter extends Component {
+  state = {
+    time : 0,
+    show : "seconds",
+  };
+
+  setTimer = (event) => {
+    this.setState({
+      time : event.target.value,
+    })
+  };
+
+  start = () =>{
+      var timer = window.setInterval( () =>{
+      this.setState({
+        time : parseInt(this.state.time-1),
+      })
+    },1000);
+  };
+  check = () => {
+    if(this.state.time < 0){
+      clearInterval(this.start.timer);
+      return "End"
+    }
+    return this.state.time+this.state.show
+  }
+  input = () => {
+    if(this.state.time < 0){
+      return 0;
+    }
+    return this.state.time;
+  }
+  
+
   render() {
     return (
       <div className="Counter">
@@ -10,11 +43,12 @@ class Counter extends Component {
         <div className = "main">
           <div className="left">
             <label htmlFor="time" className="time">设置时间</label>
-            <input type="text" className="timeText"></input>
-            <input type="submit" value="Start" ></input>
+            <input type="text" className="timeText" value={this.input()} onChange={this.setTimer}></input>
+            <input type="submit" value="Start" onClick={this.start}></input>
           </div>
           <div className="right">
-            <input type="text" className="countText" disabled="false" placeholder="Start"></input>
+            <input type="text" className="countText" disabled="false" placeholder="Start" 
+            value={this.check()}></input>
           </div>
         </div>
         <Link to = "/" className="back">回到主页</Link> 
