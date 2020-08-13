5d32a4af0233bce0fc88c6ad342c54e3e0be13e3
lemon-piepie <purplechilde@163.com>
2020-08-13 08:51:45 +0800

    calculator function and some update

--- src/app/Home.js
+++ src/app/Home.js
@@ -4,6 +4,8 @@ import imgURL2 from '../images/calculator.png';
 import imgURL3 from '../images/timer.png';
 import '../style/Home.css';
 import { Link } from 'react-router-dom';
+import Calculator from '../page/Calculator';
+import Counter from '../page/Counter';
 
 const Home = () => {
   return (
@@ -16,12 +18,12 @@ const Home = () => {
     <div className = "downPart">
       <div className = "left">
         <img src = {imgURL2}></img>
-        <Link to='/calculate' className="link">计算器</Link>
+        <Link exact to='/calculator' className="link1">计算器</Link>
       </div>
       <div className = "right">
         <img src = {imgURL3}></img>
-        <Link to='/counter' className="link">倒计时器</Link>
-      </div>    
+        <Link exact to='/counter' className="link2">倒计时器</Link>
+      </div>   
     </div>
   </div>);
 };
--- src/page/Calculator.js
+++ src/page/Calculator.js
@@ -3,37 +3,75 @@ import { Link } from 'react-router-dom';
 import '../style/Calculator.css';
 
 class Calculator extends Component {
+  state = {
+    number:0,
+    firstNum:0,
+    mark:null,
+  };
+  addNum = (event) => {
+    this.setState({
+      number: parseInt(this.state.number + event.target.value),
+    })
+  };
+  chooseMark = (event) => {
+    this.setState({
+      firstNum:this.state.number,
+      number:0,
+      mark:event.target.value,
+    })
+  }
+  calculator = () => {
+    if(this.state.mark === '+'){
+      this.setState({
+      number: parseInt(this.state.number)+this.state.firstNum,
+      });
+    }else if(this.state.mark === '-'){
+      this.setState({
+        number: this.state.firstNum-parseInt(this.state.number),
+      });
+    }else{
+      this.setState({
+        number: parseInt(this.state.number)*this.state.firstNum,
+      });
+    }
+    
+  }
+  clearNum = () => {
+    this.setState({
+      number: 0,
+    })
+  }
   render() {
     return (
       <div className="Calculator">
         <h1>在线计算器</h1>
         <div className = "main">
-          <input type="text" name="result" className="result"></input>
+          <input type="text" name="result" className="result" value={this.state.number}></input>
           <table>
             <tr>
-              <td><button value="+" className="mark">+</button></td>
-              <td><button value="-" className="mark">-</button></td>
-              <td><button value="*" className="mark">*</button></td>
+              <td><button value="+" className="mark" onClick={this.chooseMark}>+</button></td>
+              <td><button value="-" className="mark" onClick={this.chooseMark}>-</button></td>
+              <td><button value="*" className="mark" onClick={this.chooseMark}>*</button></td>
             </tr>
             <tr>
-              <td><button value="7" className="num">7</button></td>
-              <td><button value="8" className="num">8</button></td>
-              <td><button value="9" className="num">9</button></td>
+              <td><button value="7" className="num" onClick={this.addNum}>7</button></td>
+              <td><button value="8" className="num" onClick={this.addNum}>8</button></td>
+              <td><button value="9" className="num" onClick={this.addNum}>9</button></td>
             </tr>
             <tr>
-              <td><button value="4" className="num">4</button></td>
-              <td><button value="5" className="num">5</button></td>
-              <td><button value="6" className="num">6</button></td>
+              <td><button value="4" className="num" onClick={this.addNum}>4</button></td>
+              <td><button value="5" className="num" onClick={this.addNum}>5</button></td>
+              <td><button value="6" className="num" onClick={this.addNum}>6</button></td>
             </tr>
             <tr>
-              <td><button value="1" className="num">1</button></td>
-              <td><button value="2" className="num">2</button></td>
-              <td><button value="3" className="num">3</button></td>
+              <td><button value="1" className="num" onClick={this.addNum}>1</button></td>
+              <td><button value="2" className="num" onClick={this.addNum}>2</button></td>
+              <td><button value="3" className="num" onClick={this.addNum}>3</button></td>
             </tr>
             <tr>
-              <td><button value="0" className="num">0</button></td>
-              <td><button value="clear" className="clear">clear</button></td>
-              <td><button value="=" className="equal">=</button></td>
+              <td><button value="0" className="num" onClick={this.addNum}>0</button></td>
+              <td><button value="clear" className="clear" onClick={this.clearNum}>clear</button></td>
+              <td><button value="=" className="equal" onClick={this.calculator}>=</button></td>
             </tr>
           </table>
         </div>
